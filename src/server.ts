0227c1af6f2d6a8d08c5c834df7d15fa2379646e
jaonunesunb <jaonunesunb@gmail.com>
2025-08-22 19:21:11 -0300

    update

--- src/server.ts
+++ src/server.ts
@@ -3,14 +3,13 @@ import AppDataSource from "./data-source";
 import "dotenv/config";
 
 (async () => {
-  await AppDataSource.initialize()
-    .catch((err) => {
-      console.error("Error during Data Source initialization", err);
-    })
-    .then(() => {
-      console.log("Database connected!");
-      app.listen(3001, () => {
-        console.log("Servidor executando em http://localhost:3001");
-      });
+  try {
+    await AppDataSource.initialize();
+    console.log("Database connected!");
+    app.listen(3001, () => {
+      console.log("Servidor executando em http://localhost:3001");
     });
+  } catch (err) {
+    console.error("Error during Data Source initialization", err);
+  }
 })();
